8c8181164e43c9b4c19a53b6250acdcc6a7d52a6
Markus Tacker <m@tckr.cc>
2012-11-27 20:37:07 +0100

    Nun werden auch die Favoriten der Kuratoren gezogen und nicht die Favoriten der Follower.

--- publishretweets.py
+++ publishretweets.py
@@ -20,21 +20,18 @@ friends = []
 aday = datetime.now() - timedelta(1)
 
 def update_favorites(timer_id):
-    EventBus.send('log.event', "curators.list")
-    EventBus.send('curators.list', "")
+    EventBus.send('log.event', "update_favorites")
+    for curator in curators:
+        EventBus.send('log.event', "user.favorites.list")
+        EventBus.send('user.favorites.list', curator)
     
 def curators_received(message):
     global curators
     curators = json.loads(message.body)
-    EventBus.send('log.event', "friends.list")
-    EventBus.send('friends.list', "")
     
 def friends_received(message):
     global friends
     friends = json.loads(message.body)
-    for friend in friends:
-        EventBus.send('log.event', "user.favorites.list")
-        EventBus.send('user.favorites.list', friend)
         
 def favorites_received(message):
     data = json.loads(message.body)
@@ -49,8 +46,14 @@ EventBus.register_handler('curators.list.result', False, curators_received)
 EventBus.register_handler('friends.list.result', False, friends_received)
 EventBus.register_handler('user.favorites.list.result', False, favorites_received)
 
+# Initialize lists
+EventBus.send('log.event', "curators.list")
+EventBus.send('curators.list', "")
+EventBus.send('log.event', "friends.list")
+EventBus.send('friends.list', "")
+
 # Update favorites every n minutes
 vertx.set_periodic(1000 * 60 * 15, update_favorites)
 
 # Wait for first fetch
-vertx.set_timer(1000, update_favorites)
+vertx.set_timer(10000, update_favorites)
